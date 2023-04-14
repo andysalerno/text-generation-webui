7e0a68a8437f4506644af561ac36d63923b7f68b
andysalerno <andysalerno@gmail.com>
2023-04-13 18:59:04 -0700

    check stopping strings

--- extensions/api_streaming/script.py
+++ extensions/api_streaming/script.py
@@ -49,14 +49,15 @@ async def _handle_connection(websocket, path):
             'seed': int(message.get('seed', -1)),
             'add_bos_token': bool(message.get('add_bos_token', True)),
             'truncation_length': int(message.get('truncation_length', 2048)),
-            'custom_stopping_strings': [],
+            'custom_stopping_strings': message.get('custom_stopping_strings', ''),
             'ban_eos_token': bool(message.get('ban_eos_token', False))
         }
 
         generator = generate_reply(
             prompt,
             generate_params,
-            stopping_strings=message.get('stopping_strings', []),
+            stopping_strings=message.get(
+                'stopping_strings', message.get('stopping_strings', '')),
         )
 
         # As we stream, only send the new bytes.
